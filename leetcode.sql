923014ad45476ae85619e6cc38548cfd9b158be5
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-03-13 14:44:29 +0545

    Daily Active User Count

--- leetcode.sql
+++ leetcode.sql
@@ -178,6 +178,16 @@ SELECT teacher_id,count(distinct subject_Id) AS cnt
 FROM teacher t1
 group by teacher_id;
 
+--24.Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
+SELECT 
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date > '2019-07-27'::date - INTERVAL '30 days'
+  AND activity_date <= '2019-07-27'
+GROUP BY 1;
+
+
 
 
 
