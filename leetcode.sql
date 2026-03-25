4a75486591ba6c2fb23424dcd9732a03bd6ccfba
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-03-25 17:20:04 +0545

    SUBQUERIES

--- leetcode.sql
+++ leetcode.sql
@@ -322,5 +322,17 @@ AND e.manager_id IS NOT NULL
 AND m.employee_id IS NULL
 ORDER BY e.employee_id;
 
+/* 38. Write a solution to swap the seat id of every two consecutive
+students. If the number of students is odd,the id of the last student
+is not swapped. Return result table ordered by id in ascending
+order.*/
+SELECT CASE WHEN id % 2 =1 AND id+1 in ( SELECT id FROM Seat)
+THEN id+1
+WHEN id % 2 =0 THEN id-1
+ELSE id
+END AS id, student
+FROM Seat
+ORDER BY id;
+
 
 
