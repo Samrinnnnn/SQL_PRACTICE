020039fddce0066deb15bd1aaf5e9e5ec14c40e6
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-03-26 15:51:44 +0545

    CTE

--- leetcode.sql
+++ leetcode.sql
@@ -389,3 +389,20 @@ FROM all_ids
 GROUP BY id
 ORDER BY COUNT(id) DESC
 LIMIT 1
+
+/* 42. Write a solution to report the sum of all total investment values in 2016 tiv_2016, for 
+all policyholders who: have same tiv_2015 value as one or more other policyholders, and are not
+located in same city as any other policyholder(i.e (lat,lon) attribute pairs must be unique).
+Round tiv_2016 to two decimal places.*/
+WITH CheckedRows AS(
+  SELECT tiv_2016, COUNT(*) OVER (PARTITION BY tiv_2015) AS tiv_2015_count,COUNT(*) OVER
+  (PARTITIOB BY lat,lon)
+  AS loc_count
+  FROM Insurance
+  )
+SELECT ROUND(SUM(tiv_2016)::numeric,2)
+AS tiv_2016
+FROM CheckedRows
+WHERE
+tiv_2015_count>1
+AND loc_count=1;
